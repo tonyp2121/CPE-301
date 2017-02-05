0cc9a54732dfe0580c97be10e7d02be3e641e5d3
Anthony Pallone <pallone@unlv.nevada.edu>
2017-02-04 20:25:29 -0800

    Updated code, potentially finished.

--- Design Assignment 1/DA_02/main.asm
+++ Design Assignment 1/DA_02/main.asm
@@ -16,6 +16,8 @@
 	LDI R21, 0
 	LDI R23, 0				;running sum for 3
 	LDI R24, 0
+	ldi R25, 0
+	LDI R31,0
 	LDI ZL, LOW(RAMEND/2)
 	LDI ZH, HIGH(RAMEND/2)	;Have to set Z to point to RAMEND/2
 
@@ -33,37 +35,60 @@ loopOne:
 loopTwo:
 	LD R9, Z+
 	MOV R8, R9
-	CP R8, R17
-	BRLT sevenDone
-
-sevenLoop:
-	SUB R8, R17
 	CP R8, R18
 	BREQ threeSkip	;if its R8 == 0 we dont have to do any of the next calculations its just a tiny time skip
 	CP R8, R17
-	BRGE sevenLoop
+	BRLO sevenDone
+
+sevenLoop:
+	SUB R8, R17
+	CP R8, R17
+	BRSH sevenLoop
 
 sevenDone:
 	CP R8, R18
 	BRNE sevenSkip
 	ADD R20, R9
-				
+	brlo overflowSeven
+	jmp sevenSkip
+
+overflowSeven:
+	inc R21
+	ldi r31, 0b00010000
+	or	r31, r25
+	mov r7, r31
+	out DDRB, r31
+	out PORTB, r31
+			
 sevenSkip:
 	MOV R8, R9
 	CP R8, R17
-	BRLT threeLoop
+	BRLO threeDone
 
 threeLoop:
 	SUB R8, R16
 	CP R8, R16
-	BRGE threeLoop
+	BRSH threeLoop
 
 threeDone:
 	CP R8, R18
 	BRNE threeSkip
-	ADD R23, R9
+	ADD R23, R9'
+	brlo overflowThree
+	jmp threeSkip
+
+overflowThree:
+	inc r24
+	ldi r25, 0b00001000
+	or	r25, r31
+	mov r7, r25
+	out DDRB, r25
+	out PORTB, r25 
 
 threeSkip:
 	DEC R19
 	CP  R19, R18
 	BRNE loopTwo
+
+done:
+	jmp loopOne
