523e987d436801e0a7ad3eecd6dc70c620ea75c3
Anthony Pallone <pallone@unlv.nevada.edu>
2017-02-19 23:10:22 -0800

    Added Design Assingment 3

---
+++ Design Assignment 3/Design Assignment 3/Design Assignment 3/main.c
@@ -0,0 +1,20 @@
+/*
+ Author : Anthony Pallone
+ Sarah Harris
+ CPE 301L
+ Design Assignment 2_3
+*/
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	/*	
+		Pseudocode:
+		Initialize everything beforehand
+		Initialize clock interrupt
+		If counter = 1 red if 2 blue if 3 green	
+	*/
+}
+
