cdc47ca4f08b943982c3a5e4b33b7408092bc7b7
Anthony Pallone <pallone@unlv.nevada.edu>
2017-02-25 20:34:18 -0800

    Uploaded DA_04 part 1

--- Design Assignment 4/DA_04_1/DA_04_1/main.c
+++ Design Assignment 4/DA_04_1/DA_04_1/main.c
@@ -13,6 +13,7 @@ int main(void)
 		
 		
 	*/
+	float view = 0;
 	DDRB = 0xFF;		// make Port B an output
 	DDRD = 0xFF;		// make Port D an output
 	DDRC = 0x0;			// make Port C an input (for ADC input)
@@ -21,12 +22,25 @@ int main(void)
 						// data is right-justified
 	ADCSRA = 0x87;		// enable ADC, system clock used for A/D conversion
 	ADCSRB = 0x0;		// free running mode
+	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00) ; // non inverse phase correct PWM mode
+	TCCR0B = (1 << CS00) ;  // no prescaler
 	while (1) {
 		ADCSRA |= (1 << ADSC);				// start conversion
 		while ( (ADCSRA&(1<<ADIF)) == 0 ) ; // wait for conversion to finish
-		PORTD = ADCL;						// send low byte to PORTD
-		PORTB = ADCH;						// send high byte to PORTB
-	}
+		if (ADC >= 512) //forward
+		{
+			view = ADC/512*255;
+			PORTB = 0x02;
+			OCR0A = 255-ADC/4;
+			
+		}
+		else //backward
+		{
+			view = (ADC+512)/512*255;
+			PORTB = 0x01;
+			OCR0A = 255-ADC/4;
+		}
+	} 
 	return 0;
 }
 
