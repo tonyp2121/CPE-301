300ec79d28f41d448eac5d0e6bfb28dc9aff2ac0
Anthony Pallone <pallone@unlv.nevada.edu>
2017-02-28 19:25:53 -0800

    Updated code for DA 4
    Near completion of the project.

--- Design Assignment 4/DA_04_1/DA_04_1/main.c
+++ Design Assignment 4/DA_04_1/DA_04_1/main.c
@@ -3,6 +3,7 @@
  */ 
 
 #include <avr/io.h>
+#define F_CPU 8000000L
 
 
 int main(void)
@@ -24,22 +25,26 @@ int main(void)
 	ADCSRB = 0x0;		// free running mode
 	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00) ; // non inverse phase correct PWM mode
 	TCCR0B = (1 << CS00) ;  // no prescaler
+	TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);
+	TCCR1B = _BV(WGM13) | _BV(WGM12)| _BV(CS11);
+	ICR1 = (F_CPU/8)/50 -1;
 	while (1) {
 		ADCSRA |= (1 << ADSC);				// start conversion
 		while ( (ADCSRA&(1<<ADIF)) == 0 ) ; // wait for conversion to finish
+		view = ADC/4;
 		if (ADC >= 512) //forward
 		{
-			view = ADC/512*255;
-			PORTB = 0x02;
-			OCR0A = ADC/4;
+			PORTB = 0x20;
+			OCR0A = view;
 			
 		}
 		else //backward
 		{
 			view = (ADC+512)/512*255;
-			PORTB = 0x01;
-			OCR0A = 255-ADC/4;
+			PORTB = 0x10;
+			OCR0A = 255 - view;
 		}
+		OCR1B = (ADC) * 1400/1024 + 800; 
 	} 
 	return 0;
 }
