0eff08de2eeb94ac1ad346ca1541f690ec643da9
Anthony Pallone <pallone@unlv.nevada.edu>
2017-04-02 16:19:47 -0700

    Added user input going to clock to tell the time.

--- FinalProject/FinalProject/main.c
+++ FinalProject/FinalProject/main.c
@@ -78,7 +78,6 @@ int main(void)
 	RTC2_VALUE->format = RTC2_FORMAT_PM;
 	rtc2_preset(RTC2_VALUE);
 	
-	/*
 	nokia_lcd_clear();
 	nokia_lcd_set_cursor(10,20);
 	nokia_lcd_write_string("Please Enter",1);
@@ -87,29 +86,40 @@ int main(void)
 	nokia_lcd_render();
 	nokia_lcd_clear();
 	_delay_ms(2000);
-	*/
 			
 	while(1){
-			if(cursorPosition == 3){break;}
+			if(cursorPosition == 2){break;}
 			if(cursorPosition == 0){ nokia_lcd_set_cursor(12,0); nokia_lcd_write_string("/",2);  nokia_lcd_set_cursor(20,0); nokia_lcd_write_char(slash,2);}
+			if(cursorPosition == 1){ nokia_lcd_set_cursor(58,0); nokia_lcd_write_string("/",2);  nokia_lcd_set_cursor(66,0); nokia_lcd_write_char(slash,2);}
 			nokia_lcd_set_cursor(5,16);
 			if(timeHours < 10) {nokia_lcd_write_string("0",3);}
 			nokia_lcd_write_string(itoa(timeHours,buf,10), 3);
-			nokia_lcd_set_cursor(40,16);
+			nokia_lcd_write_string(":",3);
 			if(timeMinutes < 10) {nokia_lcd_write_string("0",3);}
 			nokia_lcd_write_string(itoa(timeMinutes,buf,10), 3);
 			nokia_lcd_render();
-			if ((PIND & 0x01) && (cursorPosition == 0)){timeHours++; _delay_ms(500);}
-			else if ((PIND & 0x01) && (cursorPosition == 1)){timeMinutes++; _delay_ms(100);}
-			else if (PIND & 0x02){cursorPosition++; _delay_ms(500);}
+			if ((PIND & 0x01) && (cursorPosition == 0)){timeHours++; _delay_ms(200);}
+			else if ((PIND & 0x01) && (cursorPosition == 1)){timeMinutes++; _delay_ms(200);}
+			if (PIND & 0x02){cursorPosition++; _delay_ms(200);}
+			nokia_lcd_clear();
+			if (timeMinutes >= 60){timeMinutes = 0;}
+			if (timeHours >= 24){timeHours = 0;}
 	}
+	
+	RTC2_VALUE->seconds = 0;
+	RTC2_VALUE->minutes = timeMinutes;
+	RTC2_VALUE->hours = timeHours;
+	
+	rtc2_preset(RTC2_VALUE);
 	
 	while(1){
 	drawImage(img);
 	rtc2_update(RTC2_VALUE);
 	nokia_lcd_set_cursor(6, 6);
+	if(RTC2_VALUE->hours < 10){nokia_lcd_write_string("0",1);}
 	nokia_lcd_write_string(itoa(RTC2_VALUE->hours,buf,10),1);
 	nokia_lcd_write_string(":",1);
+	if(RTC2_VALUE->minutes < 10){nokia_lcd_write_string("0",1);}
 	nokia_lcd_write_string(itoa(RTC2_VALUE->minutes,buf,10),1);
 	nokia_lcd_write_string(":",1);
 	if(RTC2_VALUE->seconds < 10){nokia_lcd_write_string("0",1);}
